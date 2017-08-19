d449006926209332d999cff5008412067705159e
GabSaldana <ela.ri.bag@outlook.com>
2017-08-19 13:15:13 -0500

    añadeindo parte 4

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -10,7 +10,7 @@ class AdminLoginController extends Controller
 {
       public function __construct(){
 
-          $this->middleware('guest:admin');// dejamos pasar personas no logeadas como admin
+          $this->middleware('guest:admin', ['except' => ['logout']]);// dejamos pasar personas no logeadas como admin
       }
 
       public function showLoginForm(){
@@ -20,18 +20,23 @@ class AdminLoginController extends Controller
 
         public function login(Request $request){
 
-        //Validate the form data
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required|min:6'
-        ]);
-        //Attempt to log the user in
-        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],$request->remember)){
-          //if success , redirect to their dashboard
-            return redirect()->intended(route('admin.dashboard'));
-        }
-        //if not redirect back to login
-        return redirect()->back()->withInput($request->only('email', 'remember'));
+            //Validate the form data
+            $this->validate($request, [
+                'email' => 'required|email',
+                'password' => 'required|min:6'
+            ]);
+            //Attempt to log the user in
+            if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],$request->remember)){
+              //if success , redirect to their dashboard
+                return redirect()->intended(route('admin.dashboard'));
+            }
+            //if not redirect back to login
+            return redirect()->back()->withInput($request->only('email', 'remember'));
 
+      }
+
+      public function logout(){
+          Auth::guard('admin')->logout();
+          return redirect('/');
       }
 }
